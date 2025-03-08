2b8e010dc69276b0a283255b59709a36e5ce4b11
Yunna Jang <yunnajang0922@gmail.com>
2025-03-08 19:33:26 +0900

    Feat(auth): Restrict access to profile page for authenticated user

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -29,10 +32,18 @@ function Header() {
               About
             </li>
           </Link>
-          <Link to='/sign-in'>
-            <li className='sm:inline text-slate-700 hover:underline'>
-              Sign in
-            </li>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className='sm:inline text-slate-700 hover:underline'>
+                Sign in
+              </li>
+            )}
           </Link>
         </ul>
       </div>
